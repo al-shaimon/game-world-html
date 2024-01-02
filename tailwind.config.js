0c444be155a281ad70d64ee8c62c3f37a9b64ee4
al-shaimon <alshaimon968@gmail.com>
2024-01-02 22:24:34 +0600

    initial setup and navbar

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FFC536',
+        secondary: '#050818',
+      },
+      fontFamily: {
+        'work-sans': ['Work Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
